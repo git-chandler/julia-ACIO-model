5832da4a9bfac3c93e045d974b58f423fd7373a6
git-chandler <63886125+git-chandler@users.noreply.github.com>
2025-07-20 23:19:08 -0500

    Uploaded first Julia code file

---
+++ acio_bmk_model.jl
@@ -0,0 +1,39 @@
+#=
+This code reads and processes data for constructing an ACIO model.
+Data are from the BEA benchmark detail I-O tables.
+=#
+
+# set working directory
+cd("/home/chandler/julia-ACIO-model/")
+julia -project="."
+
+#use_table_path = "/home/chandler/julia-ACIO-model/"
+
+# import packages
+import XLSX
+using DataFrames
+
+# read detailed 2017 use table to a data frame
+useTable = XLSX.readdata("IOUse_Before_Redefinitions_PRO_2017_Detail.xlsx", 
+                         "2017",
+                         "A6:PK414")
+
+# drop intermediate total, final demand total, and final total columns
+# drop intermediate total, value added total, and final total rows
+useTable = useTable[1:end .∉[[404, 408, 409]],1:end .∉[[405, 426, 427]]]
+
+# commodity and value added descriptions
+comDesc = useTable[1:403, 1:2]
+vaDesc = useTable[404:406,1:2]
+useTable = useTable[1:end, 1:end .≠2]
+
+# final demand table
+fdTable = useTable[1:403, 1:end .∉[2:404]]
+
+# value added table
+vaTable = useTable[404:406, 1:end]
+
+# use table
+useTable = useTable[1:403, 1:403]
+
+
