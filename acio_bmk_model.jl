63f2863f085181633ebf7070ced96738dc06d8d6
chandler <stats.chandler@gmail.com>
2025-07-30 20:40:33 -0500

    updated to improve use of data frames

--- acio_bmk_model.jl
+++ acio_bmk_model.jl
@@ -1,4 +1,5 @@
 
+cd("/home/chandler/julia-ACIO-model/")
 
 # import packages
 import XLSX
@@ -6,86 +7,94 @@ using DataFrames
 using LinearAlgebra
 
 # read detailed 2017 make table
-makeTable = XLSX.readdata("data/IOMake_Before_Redefinitions_2017_Detail.xlsx", 
-                          "2017",
-                          "A6:OO414")
+makeTable = XLSX.readtable("data/IOMake_Before_Redefinitions_2017_Detail.xlsx", 
+                           "2017",
+                           "A:OO";
+                           first_row=6,
+                           header=true)
+
+# convert to data frame
+makeDF = DataFrame(makeTable)
 
 # change missing to 0. not ideal b.c BEA keeps true
-# zeroes blank. will revisit this another time.
-makeTable = coalesce.(makeTable,0)
+# zeros blank. will revisit this another time.
+makeDF = coalesce.(makeDF,0)
 
-# reading too many rows. drop missing rows and row and column totals.
-makeTable = makeTable[1:403, 1:404]
+# drop missing rows and row and column totals.
+makeDF = makeDF[1:402, 1:404]
 
 # industry descriptions
-indDesc = DataFrame(Code=makeTable[2:end,1],IndDesc=makeTable[2:end,2])
+indDesc = makeDF[1:end,1:2]
 
 # drop description column
-makeTable = makeTable[1:end, 1:end .∉2]
-
-nind = size(makeTable)[1]-1 # number of industries
-ncom = size(makeTable)[2]-1 # number of commodities
+makeDF = makeDF[1:end, 1:end .∉2]
 
 # read detailed 2017 use table to a data frame
-useTable = XLSX.readdata("data/IOUse_Before_Redefinitions_PRO_2017_Detail.xlsx", 
-                         "2017",
-                         "A6:PK414")
+useTable = XLSX.readtable("data/IOUse_Before_Redefinitions_PRO_2017_Detail.xlsx", 
+                          "2017",
+                          "A:PK";
+                          first_row=6,
+                          header=true)
 
-useTable = coalesce.(useTable,0)
+# convert to data frame
+useDF = DataFrame(useTable)
+
+# convert missing to zero
+useDF = coalesce.(useDF,0)
 
 # drop intermediate total, final demand total, and final total columns
 # drop intermediate total, value added total, and final total rows
-useTable = useTable[1:end .∉[[404, 408, 409]],1:end .∉[[405, 426, 427]]]
+useDF = useDF[1:end .∉[[403, 407, 408]],1:end .∉[[405, 426, 427]]]
 
 # commodity and value added descriptions
-comDesc = DataFrame(Code=useTable[2:end,1],ComDesc=useTable[2:end,2])
-vaDesc = useTable[404:406,1:2]
-useTable = useTable[1:end, 1:end .≠2]
+comDesc = useDF[1:402,1:2]
+vaDesc = useDF[403:405,1:2]
+useDF = useDF[1:end, 1:end .∉2]
 
 indComDesc = outerjoin(indDescDF,comDescDF,on=:Code)
 
 # imports and final demand table
-fdTable = useTable[1:403, 1:end .∉[2:403]]
-imports = vcat(fdTable[1,9], -1*fdTable[2:end,9])
-fdTable = fdTable[:, 1:end .∉9]
+fdDF = useDF[1:402, 1:end .∉[2:403]]
+impDF = fdDF[:,9]
+fdDF = fdDF[:, 1:end .∉9]
 
 # value added table
-vaTable = useTable[1:end .∉[2:403], 1:403]
+vaDF = useDF[403:405, 1:403]
 
 # use table
-useTable = useTable[1:403, 1:403]
+useDF = useDF[1:402, 1:403]
 
 # some data checks
 
 # total commodity use by all markets
-comuse = sum(useTable[2:end,2:end], dims = 2) +
-            sum(fdTable[2:end,2:end], dims = 2)
+comuse = sum(Matrix{Int64}(useDF[:,2:end]), dims = 2) +
+            sum(Matrix{Int64}(fdDF[:,2:end]), dims = 2)
 
 # total commodity availability
-comavail = sum(makeTable[2:end,2:end], dims = 1) +
-            reshape(imports[2:end,:], (1,ncom))
+comavail = sum(Matrix{Int64}(makeDF[:,2:end]), dims = 1) +
+            -1*reshape(Vector{Int64}(impDF), (1,402))
 
 # use equals availability
 com_bal = comuse - comavail'
 com_bal[abs.(com_bal) .> 10] # small differences
 
 # total industry output
-output = sum(makeTable[2:end,2:end], dims = 2)
+output = sum(Matrix{Int64}(makeDF[:,2:end]), dims = 2)
 
 # total industry outlays
-outlays = sum(useTable[2:end,2:end], dims = 1) +
-            sum(vaTable[2:end,2:end], dims = 1)
+outlays = sum(Matrix{Int64}(useDF[:,2:end]), dims = 1) +
+            sum(Matrix{Int64}(vaDF[:,2:end]), dims = 1)
 
 # output equals outlays
 ind_bal = output - outlays' 
 ind_bal[abs.(ind_bal) .> 10] # small differences
 
 # GDI + imports
-gdiplusimp = sum(vaTable[2:end,2:end], dims = 1) +
-                reshape(imports[2:end,:], (1,ncom))
+gdiplusimp = sum(Matrix{Int64}(vaDF[:,2:end]), dims = 1) +
+                -1*reshape(Vector{Int64}(impDF), (1,402))
 
 # GDP + imports
-gdpplusimp = sum(fdTable[2:end,2:end], dims = 2)
+gdpplusimp = sum(Matrix{Int64}(fdDF[:,2:end]), dims = 2)
 
 # GDI + imports = GDP net of imports
 gdp_bal = sum(gdiplusimp) - sum(gdpplusimp) # small difference
@@ -93,21 +102,21 @@ gdp_bal = sum(gdiplusimp) - sum(gdpplusimp) # small difference
 # form the submatrices and assemble the data. 
 
 # A-by-A and C-by-C portions have all 0 entries
-abya = zeros(nind, nind)
-abya = hcat(indDesc[2:end,1], abya)
-abya = vcat(reshape(useTable[1,:], (1, 403)), abya)
-cbyc = zeros(ncom, ncom)
-cbyc = hcat(comDesc[2:end,1], cbyc)
-cbyc = vcat(reshape(makeTable[1,:], (1,403)), cbyc)
+abya = zeros(402,402)
+abya = hcat(indDesc[:,1], abya)
+abya = DataFrame(abya, names(useDF))
+cbyc = zeros(402,402)
+cbyc = hcat(comDesc[:,1], cbyc)
+cbyc = DataFrame(cbyc,names(makeDF))
 
 # transactions matrix
-T_left = vcat(abya, useTable[2:end,:])
-T_right = vcat(makeTable[:,2:end], cbyc[2:end,2:end])
-T_ = hcat(T_left, T_right)
+T_left = vcat(abya, useDF)
+T_right = vcat(makeDF[:,2:end], cbyc[:,2:end])
+T_ = hcat(T_left, T_right) # duplicate variable names
 
 # leakage vector
-l_va = sum(vaTable[2:end, 2:end], dims = 1)
-l_ = hcat("L00", l_va, reshape(imports[2:end,:], (1,nind)))
+l_va = sum(Matrix{Int64}(vaDF[:, 2:end]), dims = 1)
+l_ = hcat("L00", l_va, -1*reshape(Vector{Int64}(impDF), (1,402)))
 
 # injection vector
 x_ = sum(fdTable[2:end,2:end], dims = 2)
