2ee345880d8f0376d7598339adda5abca290df52
chandler <stats.chandler@gmail.com>
2025-08-02 20:55:26 -0500

    balance achieved

--- acio_bmk_model.jl
+++ acio_bmk_model.jl
@@ -1,4 +1,7 @@
 
+## i have dropped customs duties from the data for now.
+## i will revisit this in the future.
+## i will also make improvements to the code in the futre.
 
 # import packages
 import XLSX
@@ -22,11 +25,14 @@ makeDF = coalesce.(makeDF,0)
 # drop missing rows and row and column totals.
 makeDF = makeDF[1:402, 1:404]
 
+# drop customs duties row and column
+makeDF = makeDF[makeDF.Code.!="4200ID", names(makeDF).!="4200ID"]
+
 # industry descriptions
 indDesc = makeDF[1:end,1:2]
 
 # drop description column
-makeDF = makeDF[1:end, 1:end .∉2]
+makeDF = makeDF[1:end, 1:end .!= 2]
 
 # read detailed 2017 use table to a data frame
 useTable = XLSX.readtable("data/IOUse_Before_Redefinitions_PRO_2017_Detail.xlsx", 
@@ -45,23 +51,24 @@ useDF = coalesce.(useDF,0)
 # drop intermediate total, value added total, and final total rows
 useDF = useDF[1:end .∉[[403, 407, 408]],1:end .∉[[405, 426, 427]]]
 
-# commodity and value added descriptions
-comDesc = useDF[1:402,1:2]
-vaDesc = useDF[403:405,1:2]
-useDF = useDF[1:end, 1:end .∉2]
+# drop customs duties row and column
+useDF = useDF[useDF.Code .!= "4200ID", names(useDF).!= "4200ID"]
 
-indComDesc = outerjoin(indDescDF,comDescDF,on=:Code)
+# commodity and value added descriptions
+comDesc = useDF[1:401,1:2]
+vaDesc = useDF[402:404,1:2]
+useDF = useDF[1:end, 1:end .!=2]
 
 # imports and final demand table
-fdDF = useDF[1:402, 1:end .∉[2:403]]
+fdDF = useDF[1:401, 1:end .∉[2:402]]
 impDF = fdDF[:,9]
-fdDF = fdDF[:, 1:end .∉9]
+fdDF = fdDF[:, 1:end .!=9]
 
 # value added table
-vaDF = useDF[403:405, 1:403]
+vaDF = useDF[402:404, 1:402]
 
 # use table
-useDF = useDF[1:402, 1:403]
+useDF = useDF[1:401, 1:402]
 
 # some data checks
 
@@ -71,7 +78,7 @@ comuse = sum(Matrix{Int64}(useDF[:,2:end]), dims = 2) +
 
 # total commodity availability
 comavail = sum(Matrix{Int64}(makeDF[:,2:end]), dims = 1) +
-            -1*reshape(Vector{Int64}(impDF), (1,402))
+            -1*reshape(Vector{Int64}(impDF), (1,401))
 
 # use equals availability
 combal = comuse - comavail'
@@ -87,12 +94,12 @@ outlays = sum(Matrix{Int64}(useDF[:,2:end]), dims = 1) +
 
 # output equals outlays
 indbal = output - outlays' 
-indBalDF = DataFrame(indDesc=comDesc[:,1], output=output[:,1], outlays=outlays'[:,1], indbal=indbal[:,1])
+indBalDF = DataFrame(indDesc=indDesc[:,1], output=output[:,1], outlays=outlays'[:,1], indbal=indbal[:,1])
 indBalDF[abs.(indBalDF.indbal) .> 10,:] # small differences
 
 # GDI + imports
 gdiplusimp = sum(Matrix{Int64}(vaDF[:,2:end]), dims = 1) +
-                -1*reshape(Vector{Int64}(impDF), (1,402))
+                -1*reshape(Vector{Int64}(impDF), (1,401))
 
 # GDP + imports
 gdpplusimp = sum(Matrix{Int64}(fdDF[:,2:end]), dims = 2)
@@ -107,24 +114,24 @@ rename!(makeDF, names(makeDF[:,2:end]) .=> "C".*names(makeDF)[2:end])
 rename!(useDF, names(useDF[:,2:end]) .=> "I".*names(useDF)[2:end])
 makeDF.Code = string.("I",makeDF.Code)
 useDF.Code = string.("C",useDF.Code)
-abya = DataFrame(zeros(402,402), names(useDF[:,2:end]))
+abya = DataFrame(zeros(401,401), names(useDF[:,2:end]))
 insertcols!(abya,1,:Code=>makeDF.Code)
-cbyc = DataFrame(zeros(402,402), names(makeDF[:,2:end]))
+cbyc = DataFrame(zeros(401,401), names(makeDF[:,2:end]))
 insertcols!(cbyc,1,:Code=>useDF.Code)
 
 # transactions matrix
 T_left = vcat(abya, useDF)
 T_right = vcat(makeDF, cbyc)
-T_ = hcat(T_left, T_right[:,2:end]) # duplicate variable names
+T_ = hcat(T_left, T_right[:,2:end]) 
 
 # leakage vector
 l_va = sum(Matrix{Int64}(vaDF[:, 2:end]), dims = 1)
-l_ = hcat("L00", l_va, -1*reshape(Vector{Int64}(impDF), (1,402)))
+l_ = hcat("L00", l_va, -1*reshape(Vector{Int64}(impDF), (1,401)))
 l_ = DataFrame(l_,names(T_))
 
 # injection vector
 x_ = sum(Matrix{Int64}(fdDF[:,2:end]), dims = 2)
-x_ = vcat(zeros(402,1), x_, 0)
+x_ = vcat(zeros(401,1), x_, 0)
 x_ = DataFrame(x_,:auto)
 rename!(x_,[:"X00"])
 
@@ -133,8 +140,8 @@ acio = vcat(T_, l_)
 acio = hcat(acio, x_)
 
 # account balance tests
-row_s = sum(Matrix{Int64}(acio[1:804,2:end]), dims = 2)
-col_s = sum(Matrix{Int64}(acio[:,2:805]), dims = 1)
+row_s = sum(Matrix{Int64}(acio[1:802,2:end]), dims = 2)
+col_s = sum(Matrix{Int64}(acio[:,2:803]), dims = 1)
 acctbal = row_s-col_s' 
 acctBalDF = DataFrame(Desc=T_[:,1], row_s=row_s[:,1], col_s=col_s'[:,1], acctbal=acctbal[:,1])
 acctBalDF[abs.(acctBalDF.acctbal) .> 10,:] # small differences
@@ -145,7 +152,7 @@ acctBalDF[abs.(acctBalDF.acctbal) .> 10,:] # small differences
 # in T_, it's considered Matrix{Any}.
 
 # gross output vector (lower case y_)
-y_ = sum(Matrix{Int64}(acio[1:804,2:end]), dims = 2) 
+y_ = sum(Matrix{Int64}(acio[1:802,2:end]), dims = 2) 
 
 # inverse diagonal matrix of gross output (capital Y_)
 Y_ = inv(diagm(vec(y_.+eps(Float64))))
@@ -154,7 +161,7 @@ Y_ = inv(diagm(vec(y_.+eps(Float64))))
 A_ = Matrix{Float64}(T_[1:end,2:end])*Y_
 
 # total requirements multiplier matrix
-M_ = inv(Matrix{Int64}(I,804,804)-(A_))
+M_ = inv(Matrix{Int64}(I,802,802)-(A_))
 
 # value added and imports multiplier vector
 v_ = Y_ * Matrix{Float64}(l_[:,2:end])'
@@ -162,24 +169,15 @@ v_ = Y_ * Matrix{Float64}(l_[:,2:end])'
 # test the model
 
 # total requirements times final demand sums reproduces gross output
-balM_ = M_*x_[1:804,1] - y_
-balM_[abs.(balM_) .> 10] # doesn't balance
-
-imbal = balM_[abs.(balM_) .> 10]
-imbal_index = findall(x -> x == imbal[1], balM_)
-indDesc[imbal_index[1][1]+1, :]
-
-## 4200ID customs duties
-
-useTable[1, imbal_index[1][1]+1]
-y_[imbal_index[1][1]]
-(M_*x_[2:805,:])[imbal_index[1][1]]
+balM_ = M_*x_[1:802,1] - y_
+balMDF = DataFrame(Desc=T_[:,1],TrFd=M_*x_[1:802,1][:,1],Go=y_[:,1],bal=balM_[:,1])
+balM_[abs.(balM_) .> 10] # balance
 
 # gross output times value added reproduces GDI + imports
-balv_ = y_'*v_ - ones( 1,(ncom+nind))*l_[:,2:end]'
-balv_ 
+balv_ = y_'*v_ - ones(1,802)*Matrix{Int64}(l_[:,2:end])'
+balv_ # balance
 
 # walras's law
-balW = sum(v_'*M_*x_[2:805,:]) - sum(x_[2:805,:])
+balW = sum(v_'*M_*x_[1:802,1]) - sum(x_[1:802,1])
 balW # small difference
 
